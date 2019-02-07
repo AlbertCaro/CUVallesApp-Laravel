f287ef7731383d4abab65aaf4f1aa20505d6ee56
Alberto Caro Navarro <albertcaronava@gmail.com>
2019-02-07 16:40:35 -0600

    Archivos necesarios para la API de login.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\APILoginRequest;
 use Illuminate\Http\Request;
+use SoapClient;
 
 class ApiController extends Controller
 {
+    function login(APILoginRequest $request)
+    {
+        $loginService = new SoapClient(env('APP_URL').':8000/siiauwebservice.xml');
+
+        $respuesta = $loginService->valida($request->code, $request->nip,'UdGSIIAUWebServiceValidaUsuario');
+
+        dd($respuesta);
+    }
+
     function checkIn(Request $request)
     {
 
